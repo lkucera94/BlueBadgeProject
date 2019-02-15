26d851a2abad358adfebb3b7b33bc50bf5c64416
lkucera94 <lekucera94@gmail.com>
2019-02-15 15:55:30 -0500

    CSS changes

--- MilitaryBaseRater.MVC/Controllers/HomeController.cs
+++ MilitaryBaseRater.MVC/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace MilitaryBaseRater.MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "";
 
             return View();
         }
